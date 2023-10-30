8fd56cd8d01383074e15356f4515b59174ce75c6
Curso de JS <matheuschiimenndes@gmail.com>
2023-10-30 11:14:45 -0300

    Incrementando responsividade na pagina

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,42 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { dataFake } from '../../data/dataFake'
-
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
+  [x: string]: any;
 
   @Input()
-  contentPhotoCover:string= ""
+  contentPhotoCover: string = '';
   @Input()
-  contentTitle:string= "Noticias"
+  contentTitle: string = '';
   @Input()
-  contentDescription:string= ""
-  private id:string | null ="0"
+  contentDescription: string = '';
+  private id: string | null = '0';
 
-  constructor(
-    private route:ActivatedRoute
-  ){}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void{
-    this.route.paramMap.subscribe( value =>
-      this.id = value.get("id")
-    )
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
 
-    this.setValuesToComponent(this.id)
+    this.setValuesToComponent(this.id);
   }
 
-  setValuesToComponent(id:string | null){
-    const result = dataFake.filter(article => article.id == id)[0]
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id == id)[0];
 
-   this.contentTitle = result.title
-   this.contentDescription = result.description
-   this.contentPhotoCover = result.photoCover
-
+    this.contentPhotoCover = result.photoCover;
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
   }
-
 }
