80dd3ceba5b7cb7a118f1568a66385abaa989285
Curso de JS <matheuschiimenndes@gmail.com>
2023-11-15 19:23:40 -0300

    Deploy github via pasta DOCS 2

--- src/app/Service/marvel-api.service.ts
+++ src/app/Service/marvel-api.service.ts
@@ -12,8 +12,8 @@ export class MarvelAPIService {
   constructor(private https:HttpClient) { }
 
 
-  BASE_URL = `http://gateway.marvel.com/v1/public/events?limit=4&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
-  BASE_URL_Characters = `http://gateway.marvel.com/v1/public/characters?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
+  BASE_URL = `https://gateway.marvel.com/v1/public/events?limit=4&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
+  BASE_URL_Characters = `https://gateway.marvel.com/v1/public/characters?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
 
   allCharacters():Observable<any>{
     return this.https.get(this.BASE_URL_Characters);
@@ -28,12 +28,12 @@ export class MarvelAPIService {
   }
   getComicsByCharacter(characterId:string):Observable<any>
   {
-    const comicByCharacterUrl = `http://gateway.marvel.com/v1/public/characters/${characterId}/comics?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
+    const comicByCharacterUrl = `https://gateway.marvel.com/v1/public/characters/${characterId}/comics?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
     return this.https.get(comicByCharacterUrl);
   }
   getSeriesByCharacter(characterId:string):Observable<any>
   {
-    const seriesByCharacterUrl = `http://gateway.marvel.com/v1/public/characters/${characterId}/series?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
+    const seriesByCharacterUrl = `https://gateway.marvel.com/v1/public/characters/${characterId}/series?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
     return this.https.get(seriesByCharacterUrl);
   }
 }
