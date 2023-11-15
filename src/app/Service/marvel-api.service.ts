0b2dfa5893669606a1d6c80ea955924b4a943739
Curso de JS <matheuschiimenndes@gmail.com>
2023-11-15 19:17:26 -0300

    Atualizacao do HTTP para HTTPS

--- src/app/Service/marvel-api.service.ts
+++ src/app/Service/marvel-api.service.ts
@@ -9,31 +9,31 @@ import { Observable } from 'rxjs';
 export class MarvelAPIService {
 
 
-  constructor(private http:HttpClient) { }
+  constructor(private https:HttpClient) { }
 
 
   BASE_URL = `http://gateway.marvel.com/v1/public/events?limit=4&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
   BASE_URL_Characters = `http://gateway.marvel.com/v1/public/characters?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
 
   allCharacters():Observable<any>{
-    return this.http.get(this.BASE_URL_Characters);
+    return this.https.get(this.BASE_URL_Characters);
   }
   allEvents():Observable<any>{
-    return this.http.get(this.BASE_URL);
+    return this.https.get(this.BASE_URL);
   }
 
   getCharactersById(characterId: number): Observable<any>{
     const url = `${this.BASE_URL_Characters}&id=${characterId}`
-    return this.http.get(url);
+    return this.https.get(url);
   }
   getComicsByCharacter(characterId:string):Observable<any>
   {
     const comicByCharacterUrl = `http://gateway.marvel.com/v1/public/characters/${characterId}/comics?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
-    return this.http.get(comicByCharacterUrl);
+    return this.https.get(comicByCharacterUrl);
   }
   getSeriesByCharacter(characterId:string):Observable<any>
   {
     const seriesByCharacterUrl = `http://gateway.marvel.com/v1/public/characters/${characterId}/series?limit=10&ts=1&apikey=4fc8c4f6d63d9226fc6f51719478d523&hash=ee026b7f3268f97d5756e670c265d297`
-    return this.http.get(seriesByCharacterUrl);
+    return this.https.get(seriesByCharacterUrl);
   }
 }
